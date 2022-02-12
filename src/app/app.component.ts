750b65c225ff3922193c3f0b4a390fac273fda1f
Victor Hugo Bernardino <vhugoz1@hotmail.com>
2022-02-12 16:58:27 -0300

    Fazendo componente de botão

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'EmpDepManagement';
+  activeButton:boolean[] = [false, false];
+
+  changeButton(i: number){
+    switch(i) {
+      case 0:
+        this.activeButton = [true, false];
+        break;
+      case 1:
+        this.activeButton = [false, true];
+        break;
+      default:
+        this.activeButton = [false, false];
+        break;
+    }
+    
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ApiService } from './shared/api.service';
+import ApiService from './shared/api.service';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavbarComponent,
+    ButtonComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    NgbModule,
   ],
   providers: [ApiService],
   bootstrap: [AppComponent],
