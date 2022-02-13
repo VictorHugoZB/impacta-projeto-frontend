458154a8e2107636c6052f339c36bea974938555
Victor Hugo Bernardino <vhugoz1@hotmail.com>
2022-02-13 13:47:12 -0300

    Fazendo pagina dos funcionários

--- src/app/components/employee/employee.component.ts
+++ src/app/components/employee/employee.component.ts
@@ -1,15 +1,115 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { faPen, faSearch, faSort, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { Employee } from 'src/app/models/Employee';
+import ApiService from 'src/app/shared/api.service';
 
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
   styleUrls: ['./employee.component.css']
 })
-export class EmployeeComponent implements OnInit {
+export class EmployeeComponent {
 
-  constructor() { }
+  constructor(private service: ApiService) { }
+
+  faTrash = faTrash;
+  faPen = faPen;
+  faSort = faSort;
+  faSearch = faSearch;
+
+  employeeList: Employee[] = [];
+  employeeFilteredList: Employee[] = [];
+  selectedEmployee!: Employee;
+
+  loading: boolean = false;
+  responseMessage: string = '';
+
+  orderStep: number = 0;
+  filterText: string = "";
 
   ngOnInit(): void {
+    this.initializeEmpList();
+  }
+
+  initializeEmpList = () => {
+    this.service.getEmpList().subscribe((emps) => {
+      this.employeeList = [...emps];
+      this.employeeFilteredList = [...emps];
+    });
+  }
+
+  setSelectedEmployee = (emp: Employee) => {
+    this.selectedEmployee = emp;
+  }
+
+  onDismiss = () => {
+    this.responseMessage = '';
+  }
+
+  nextOrderStep = () => {
+    if (this.orderStep === 2){
+      this.orderStep = 0;
+      return;
+    }
+    this.orderStep++;
+  }
+
+  order = (field: keyof Employee) => {
+    if (this.orderStep === 0){
+      this.employeeFilteredList = this.employeeFilteredList
+        .sort((a, b) => {
+          let aconv = a[field];
+          let bconv = b[field];
+          if (typeof aconv === 'string'){
+            aconv = aconv.toString().toLowerCase();
+            bconv = bconv.toString().toLowerCase();
+          }
+          if (aconv > bconv) return 1;
+          if (aconv < bconv) return -1;
+          return 0;
+        })
+      this.nextOrderStep();
+    } else if (this.orderStep === 1){
+      this.employeeFilteredList = this.employeeFilteredList
+        .sort((a, b) => {
+          let aconv = a[field];
+          let bconv = b[field];
+          if (typeof aconv === 'string'){
+            aconv = aconv.toString().toLowerCase();
+            bconv = bconv.toString().toLowerCase();
+          }
+          if (aconv > bconv) return -1;
+          if (aconv < bconv) return 1;
+          return 0;
+        })
+      this.nextOrderStep();
+    } else if (this.orderStep === 2) {
+      this.employeeFilteredList = [...this.employeeList];
+      this.nextOrderStep();
+    }
+  }
+
+  filter = () => {
+    const filterText = this.filterText;
+    console.log(filterText);
+    this.employeeFilteredList = this.employeeList.filter((emp) => {
+      let includes = false;
+      Object.keys(emp).forEach((key) => {
+        if (emp[key as keyof Employee].toString().toLowerCase().trim().includes(filterText.toString().toLowerCase().trim())){
+          includes = true;
+        }
+      })
+      return includes;
+    })
+  };
+
+  deleteEmployee = (emp: Employee) => {
+    this.loading = true;
+    this.service.deleteEmployee(emp.EmployeeId).subscribe((res) => {
+      this.loading = false;
+      this.responseMessage = res.toString();
+      this.initializeEmpList();
+    })
   }
 
 }
